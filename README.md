542d8f5f7edb02a227249cfd4ce3daeeae924776
LeslieM98 <leslie.marxen98@gmail.com>
2018-10-16 17:54:15 +0200

    Enabled javadoc generation

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Simple compiler for the C-- programming language
+To generate JavaDoc enter `mvn javadoc:javadoc` the generated doc will be found in `./target/site/index.html`
--- src/main/java/cmm/compiler/App.java
+++ src/main/java/cmm/compiler/App.java
@@ -1,12 +1,25 @@
 package cmm.compiler;
 
+/**
+ * @author Leslie Marxen
+ * @version 1.0
+ */
+
 public class App{
 
+    /**
+     * Program entry.
+     * @param args Command line 
+    */
     public static void main(String[] args){
         App instance = new App();
         instance.start(args);
     }
 
+    /**
+     * Actual program logic.
+     * @param args contains command line arguments just like in a main method
+     */
     public void start(String[] args){
         System.out.println("Hello World!");
     }
