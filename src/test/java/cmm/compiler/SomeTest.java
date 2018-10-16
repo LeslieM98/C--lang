d129e2ea91c4eaf796b6d54e3bcb3bcc17544554
LeslieM98 <leslie.marxen98@gmail.com>
2018-10-16 19:08:22 +0200

    updated readme

--- src/test/java/cmm/compiler/SomeTest.java
+++ src/test/java/cmm/compiler/SomeTest.java
@@ -9,11 +9,6 @@ import org.junit.jupiter.api.*;
 public class SomeTest{
     @Test
     public void someTestMethod(){
-        try{
-            TimeUnit.SECONDS.sleep(20);
-        } catch (Exception e){
-            
-        }
         Assertions.assertTrue(true);
     }
 }
